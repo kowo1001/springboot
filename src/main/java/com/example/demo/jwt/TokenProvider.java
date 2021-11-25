d5ffb42a74d444d42ff09b346cdc92fc4e801ee0
JangJongWook <37354978+kowo1001@users.noreply.github.com>
2021-11-25 18:04:20 +0900

    jwt2

--- src/main/java/com/example/demo/jwt/TokenProvider.java
+++ src/main/java/com/example/demo/jwt/TokenProvider.java
@@ -1,22 +1,31 @@
 package com.example.demo.jwt;
 
+import java.security.Key;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.example.demo.entity.User;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.lang.Arrays;
 
 public class TokenProvider {
 
     public static final String AUTHORITIES_KEY = "auth";
     private final String secret;
     private final long tokenValidityInMilliseconds;
+    private Key key;
 
     public TokenProvider(@Value("${jwt.secret}") String secret,
             @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds) {
@@ -40,4 +49,16 @@ public class TokenProvider {
                 .claim(AUTHORITIES_KEY, authorities).signWith(SignatureAlgorithm.HS512, key).setExpiration(validity)
                 .compact();
     }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+
+        Collection<? extends GrantedAuthority> authorities = Arrays.stream(claims.get(AUTHORITIES_KEY).toString()
+                .split(",").map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
 }
